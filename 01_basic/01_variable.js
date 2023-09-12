1840777e890d80a20e8d7b1ee774f471cd6c5e41
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-12 04:38:28 +0000

    variable declear and variable initialization

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,8 @@
+const accoutId=24454;
+let accoutEmail="taiyab1554@gmail.com";
+var accoutPassword=5454;
+console.log(accoutId);
+console.log(accoutEmail);
+console.log(accoutPassword);
+// cosole tabular form
+console.table([accoutId,accoutEmail,accoutPassword])
