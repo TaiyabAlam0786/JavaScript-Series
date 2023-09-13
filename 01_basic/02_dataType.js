655fcbe549f4e9ed47831429ecef1c4d7b0fde90
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-13 01:45:27 +0000

    Define a data type and values

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,14 @@
+"use strict"; //treat all js code as newer version
+let age=20; //number
+let name="Taiyab"; // string
+let isloggedIn=false; //boolean
+let state; //undefined
+// Data Types
+// number
+// bigInt
+// string
+// boolean
+// null
+// undefined
+// symbol
+console.log(typeof age);
