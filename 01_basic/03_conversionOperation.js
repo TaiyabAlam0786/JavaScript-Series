c152e1361bf9e389332f1f4cb5c52d252d6f9460
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-13 02:51:32 +0000

    Operation on operand

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,6 +1,6 @@
-let value="20abcd";
-console.log(typeof value);
-let valueInNumber=Number(value)
+let values="20abcd";
+console.log(typeof values);
+let valueInNumber=Number(values)
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
 // boolean value 1 is convert to true
@@ -12,3 +12,35 @@ let number=55;
 let stringNum=String(number);
 console.log(stringNum);
 console.log(typeof stringNum);
+
+// ***** Operations *****
+let value=3;
+let negValue=-value;
+console.log(negValue);
+// Arithmetical operations
+/*
+console.log(5+5);
+console.log(5-5);
+console.log(5*5);
+console.log(5/5);
+console.log(5**3);
+console.log(5%5);
+*/
+// string concatenation
+let str1="Hello";
+let str2=" Taiyab";
+// normal concatenate
+let str3=str1+str2;
+console.log(str3)
+// concate using method but same output
+str3=str1.concat(str2);
+console.log(str3)
+// increment operator
+let x=3;
+let y=x++;
+console.log(`x:${x}, y:${y}`);
+// output:"x:4, y:3"
+let a=3;
+let b=++a;
+console.log(`a:${a}, b:${b} `)
+// output:"a:4, b:4"
