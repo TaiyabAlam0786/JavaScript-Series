9e69ebdd2d222f14bae204b908693cda1519eb24
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-13 02:10:46 +0000

    Conversion of data types

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,14 @@
+let value="20abcd";
+console.log(typeof value);
+let valueInNumber=Number(value)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+// boolean value 1 is convert to true
+let boolean=1;
+let booleanIsLoggedIn=Boolean(boolean);
+console.log(booleanIsLoggedIn);
+// number convert to string
+let number=55;
+let stringNum=String(number);
+console.log(stringNum);
+console.log(typeof stringNum);
