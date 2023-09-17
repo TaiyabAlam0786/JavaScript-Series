65bbba96137c08a3d4c153c89466b737390d63b5
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-17 02:02:50 +0000

    Some example of IIFE

---
+++ 02_basic/06_iife.js
@@ -0,0 +1,8 @@
+// Immediately Invoked Function Expressions(IIFE)
+(function show(name){
+    console.log(`Welcome to ${name}`);
+})('Taiyab');
+// Arrow
+( (fullName)=>{
+    console.log(`Welcome to ${fullName}`);
+})('Taiyab Alam');
