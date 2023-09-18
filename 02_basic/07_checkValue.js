9114aa20ee0262b6280219b1d204d518a298566b
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-09-18 03:02:02 +0000

    check empty object and array

---
+++ 02_basic/07_checkValue.js
@@ -0,0 +1,10 @@
+// check array empty
+const arr=[];
+if(arr.length===0){ 
+    console.log("Array is empty");
+}
+// check object empty
+const user={};
+if(Object.keys(user).length===0){
+    console.log("Object is empty");
+}
