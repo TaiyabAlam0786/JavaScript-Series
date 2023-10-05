d5def0a69d6910fade253bf5dae4cb4b724396dc
Taiyab Alam <107677010+TaiyabAlam0786@users.noreply.github.com>
2023-10-05 03:11:49 +0000

    Some add example

--- 05_advanced/promise.js
+++ 05_advanced/promise.js
@@ -1,68 +1,92 @@
 // Promise in js
-const promise = new Promise((resolve,reject)=>{
+const promise = new Promise((resolve, reject) => {
     // The perform async task
-    setTimeout(()=>{
+    setTimeout(() => {
         console.log('Async task is completed');
         resolve()
-    },2000);
-   
+    }, 1000);
+
 
 });
-promise.then((resolve)=>{
+promise.then((resolve) => {
     console.log(' Promise is fulfilled');
-}).catch((reject)=>{
+}).catch((reject) => {
     console.log(reject);
 
 })
 
-new Promise((resolve,reject)=>{
-    setTimeout(function(){
+new Promise((resolve, reject) => {
+    setTimeout(function () {
         console.log('Async task 2 is completed');
         resolve();
-    },3000);
-    
-}).then(function(){
+    }, 2000);
+
+}).then(function () {
     console.log('Async task 2 is fulfuilled');
 });
 
-const promise3 = new Promise(function(resolve,reject){
-    setTimeout(function(){
+const promise3 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         console.log('Async task 3 is completed');
-        resolve({user:"Taiyab Alam",email:"taiyab1253exapmle@gmail.com",password:"12452dsdty"});
-    },1000)
+        resolve({ user: "Taiyab Alam", email: "taiyab1253exapmle@gmail.com", password: "12452dsdty" });
+    }, 3000)
 });
-promise3.then(function(user){
+promise3.then(function (user) {
     console.log(user);
 });
 
-const promise4 = new Promise(function(resolve,reject){
-    setTimeout(function(){
+const promise4 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
         let error = false;
-         if( !error){
-            resolve({user:"Taiyab Alam",email:"taiyab1253exapmle@gmail.com",password:"12452dsdty"});
-         }
-         else{
+        if (!error) {
+            resolve({ user: "Taiyab Alam", email: "taiyab1253exapmle@gmail.com", password: "12452dsdty" });
+        }
+        else {
             reject('ERROR: Something went wrong')
         }
-    },5000)
+    }, 4000)
 });
-promise4.then((user)=>{
+promise4.then((user) => {
     return user.email;
-}).then((users)=>{
+}).then((users) => {
     console.log(`user Email: ${users}`);
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error);
-})
+});
+
+const promise5 = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        let error = true;
+        if (!error) {
+            resolve({ name: "Taiyab", location: "Bihar" });
+        }
+        else {
+            reject('ERROR:Something went wrong');
+        }
+
+    }, 5000)
+});
+
+async function consumePromise5() {
+    try {
+        const response = await promise5;
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+}
+consumePromise5();
 
 // API call
+console.log('API Call');
 const requestUrl = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
-fetch(requestUrl).then((response)=>{
-return response.json();
-}).then((data)=>{
+fetch(requestUrl).then((response) => {
+    return response.json();
+}).then((data) => {
     console.log(data);
-}).catch((error)=>{
+}).catch((error) => {
     console.log(error);
-}).finally(()=>{
+}).finally(() => {
     console.log('The promise resolve or reject');
 
 });
